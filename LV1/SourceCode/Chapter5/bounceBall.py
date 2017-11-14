54227f743f4688fd995601fbd5cf2a1acebe9a93
Seongki Shin <skshin@nbreds.com>
2017-11-14 14:27:35 +0900

    현재 화면 기준으로 공이 경계선에서 튀게 만듬

--- LV1/SourceCode/Chapter5/bounceBall.py
+++ LV1/SourceCode/Chapter5/bounceBall.py
@@ -1,23 +1,28 @@
 # base source : https://stackoverflow.com/questions/29158220/tkinter-understanding-mainloop
 # tkinter ref : http://infohost.nmt.edu/tcc/help/pubs/tkinter/web/index.html
 from tkinter import *
-import random
-import time
+import math
+
+canvas_width = 600
+canvas_height = 400
 
 root = Tk()
 root.title = "Game"
 #root.resizable(0,0)
 #root.wm_attributes("-topmost", 1)
 
-canvas = Canvas(root, width=500, height=400, bd=0, highlightthickness=0)
+canvas = Canvas(root, width=canvas_width, height=canvas_height, bd=0, highlightthickness=0)
 canvas.pack()
 
 
 class Ball:
     def __init__(self, canvas, color):
+        self.x = canvas_width / 2
+        self.y = canvas_height / 2
+        self.direction = 45.0
         self.canvas = canvas
-        self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
-        self.canvas.move(self.id, 245, 100)
+        self.id = canvas.create_oval(-10, -10, 10, 10, fill=color)
+        self.canvas.move(self.id, self.x, self.y)
 
         self.canvas.bind("<Button-1>", self.canvas_onclick)
         self.text_id = self.canvas.create_text(300, 200, anchor='se')
@@ -29,9 +34,36 @@ class Ball:
             text="You clicked at ({}, {})".format(event.x, event.y)
         )
 
+    def move(self):
+        self.x = 5.0 * math.cos(math.radians(self.direction))
+        self.y = 5.0 * math.sin(math.radians(self.direction))
+
     def draw(self):
-        self.canvas.move(self.id, 0, -1)
-        self.canvas.after(50, self.draw)
+        self.move()
+        #(self.canvas.coords(self.id)[0] + self.canvas.coords(self.id)[2]) / 2
+        self.canvas.move(self.id, self.canvas.canvasx(self.x), self.canvas.canvasy(self.y))
+        pos = self.canvas.coords(self.id)
+        if pos[0] < 0:
+            if self.direction < 180:
+                self.direction -= 90
+            else:
+                self.direction += 90
+        if pos[1] < 0:
+            if self.direction > 270:
+                self.direction -= 90
+            else:
+                self.direction -= 90
+        if pos[2] > canvas_width:
+            if self.direction > 180:
+                self.direction -= 90
+            else:
+                self.direction += 90
+        if pos[3] > canvas_height:
+            if self.direction > 90:
+                self.direction += 90
+            else:
+                self.direction = 360 - self.direction
+        self.canvas.after(20, self.draw)
 
 
 ball = Ball(canvas, "red")
